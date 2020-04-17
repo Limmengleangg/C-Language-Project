75e721bd463537026c3018e2c91cce229797c482
Limmengleangg <63393977+Limmengleangg@users.noreply.github.com>
2020-04-17 17:24:29 +0700

    Re-edit
    re-edit

--- Project C/moneyDeposit and moneyWithdraw.c
+++ Project C/moneyDeposit and moneyWithdraw.c
@@ -20,7 +20,28 @@ float moneyWithdraw(float balance) //Vatanak
     float withdraw;
     bool back = true;
 
+    printf("You choose to Withdraw a money\n");
+    printf("$$$$Your Balance is: $%.2f\n\n", balance);
+
+    while (back) {
+    printf("Enter your amount to withdraw:\n");
+    scanf("%f", &withdraw);
 
 
+        if (withdraw < balance) {
+            back = false;
+            balance -= withdraw;
+            printf("\n$$$$Your withdrawing money is:  $%.2f\n", withdraw);
+            printf("****Your New Balance is:   $%.2f\n\n", balance);
+
+        }
+        else  {
+            printf("+++You don't have enough money+++\n");
+            printf("Please contact to your Bank Customer Services\n");
+            printf("****Your Balance is:   $%.2f\n\n", balance);
+
+        }
+    }
+    return balance;
 
 }//money withdraw
